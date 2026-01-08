22035d1f3b7d4fb05eaa9f4dd60f6f5131220a24
Seu Nome <roquew890@gmail.com>
2026-01-08 15:29:22 -0300

    fixed

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu = document.getElementById("menu")
+const btnwhats = document.getElementById("btnwhats")
+menu.addEventListener("click", ()=>{
+    const menu_lateral = document.getElementById("menulateral")
+    const links = document.querySelectorAll("a");
+
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    menu_lateral.style.display = "none";
+  });
+});
+
+    menu_lateral.style.display = "flex"
+})
+btnwhats.addEventListener("click", ()=>{
+    let msg = `Quero um site com este! `
+    let numero = "5527997230221"
+    const link = `https://api.whatsapp.com/send?phone=${numero}&text=${msg}`;
+     window.open(link, "_blank");
+})
